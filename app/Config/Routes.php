ff8fd8e9660b9646f1b9881ffcf5a7113726bbc6
PKL (GLA) <friscafebrianti16@gmail.com>
2023-11-28 10:12:24 +0700

    add Film

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,13 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+// => default controller
+$routes->get('dashboard', 'DashboardController::index',['as' => 'dashboard']);
+
+$routes->get('/film', 'FilmController::index',['as' => 'film']);
+$routes->get('/film/edit/(:num)', 'FilmController::edit/$1',['as' => 'film-edit']);
+$routes->get('/film/delete/(:num)', 'FilmController::delete/$1',['as' => 'film-delete']);
+$routes->get('/film/create', 'FilmController::create',['as' => 'film-create']);
+$routes->post('/film/store', 'FilmController::store',['as' => 'film-store']);
+$routes->post('/film/update', 'FilmController::update',['as' => 'film-update']);
---
+++ app/Controllers/FilmController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use App\Models\FilmModel;
+
+class FilmController extends BaseController
+{
+    protected $FilmModel;
+
+    public function __construct()
+    {
+        $this->FilmModel = new FilmModel();
+    }
+
+    public function index()
+    {
+        $data = [
+            'title' => 'Film List Management',
+            'page_title' => 'Film List',
+            'films' => $this->FilmModel->findAll()
+        ];
+        return view('film/index', $data);
+    }
+
+    public function create()
+    {
+        $data = [
+            'title' => 'Film List Management',
+            'page_title' => 'Create Film List',
+        ];
+
+        return view('film/create', $data);
+    }
+
+    public function store()
+    {
+        $nama_film = $this->request->getPost('nama_film');
+        $deskripsi = $this->request->getPost('deskripsi');
+
+        $new_film = [
+            'nama_film' => $nama_film,
+            'deskripsi' => $deskripsi,
+        ];
+
+        $insert_film = $this->FilmModel->insert($new_film);
+        return redirect()->to('film');
+    }
+
+    public function edit($film_id)
+    {
+        $data = [
+            'title' => 'Film List Management',
+            'page_title' => 'Edit Film',
+            'film' => $this->FilmModel->find($film_id)
+        ];
+        return view('film/edit', $data);
+    }
+
+    public function update()
+    {
+        $film_id = $this->request->getPost('film_id');
+        $nama_film = $this->request->getPost('nama_film');
+        $deskripsi = $this->request->getPost('deskripsi');
+
+        $edit_film = [
+            'nama_film' => $nama_film,
+            'deskripsi' => $deskripsi,
+        ];
+
+        $update_film = $this->FilmModel->update($film_id, $edit_film);
+        return redirect()->to('film');
+    }
+
+    public function delete($film_id)
+    {
+        $this->FilmModel->delete($film_id);
+        return redirect()->to('film');
+    }
+}
